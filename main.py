18297723e4e81ecb7309276344e66a13cdbe0e7b
Meolsei <furrstic@gmail.com>
2023-10-09 08:16:02 -0400

    funny prefix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 intents = discord.Intents.default()
-client = discord.Client(intents = intents)
+client = commands.Bot(command_prefix="sudo ")
 
 @client.event
 async def on_ready():
