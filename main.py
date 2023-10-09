a0511e7b3e3e1c1af37dfe55a315d1cf56d56298
Meolsei <furrstic@gmail.com>
2023-10-09 08:11:55 -0400

    updated ready line

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ client = discord.Client(intents = intents)
 
 @client.event
 async def on_ready():
-    print(f'Ready!\nThe bot is up and running; {client.user}')
+    print(f'Ready!\nThe bot is up and running; {client.user.name}')
 
     for extension in EXTENSIONS:
         await client.load_extension(extension)
